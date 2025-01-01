fd332dab8d33cf0f72fa5a0ea9b44a957e57fc22
Martina-C8 <casettamartina@outlook.it>
2025-01-01 22:18:27 +0100

    speedinsights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import TodoApp from "./Components/TodoApp";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <div className="App">
+      <SpeedInsights />
       <TodoApp />
     </div>
   );
