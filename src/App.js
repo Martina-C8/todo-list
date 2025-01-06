86551f05e9759e8fe97ada4d5ce2679fc3dcfd37
Martina-C8 <casettamartina@outlook.it>
2025-01-06 22:17:46 +0100

    remove speedinsights

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import TodoApp from "./Components/TodoApp";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <div className="App">
-      <SpeedInsights />
       <TodoApp />
     </div>
   );
